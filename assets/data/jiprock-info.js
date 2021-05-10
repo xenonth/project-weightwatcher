c918f78d49fa7b5acddc1f1add353e597b69416b
Timothy Forbes <timothy.forbes259@gmail.com>
2021-05-10 13:13:00 +0930

    intial outline setup

---
+++ assets/data/jiprock-info.js
@@ -0,0 +1,10 @@
+class Jiprock {
+    constructor (dimensions, weight) {
+        this.dimensions = dimensions;
+        this.weight = weight;
+    }
+
+    getWeight () {
+        return this.weight
+    }
+}
