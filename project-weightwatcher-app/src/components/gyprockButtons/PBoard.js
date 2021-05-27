8b71a51fc5f7fa9e992e6253fec3bd6fb6bec48e
Timothy Forbes <timothy.forbes259@gmail.com>
2021-05-27 10:27:13 +0930

    Site now has multiplier functionality just need to convert it to a modal format.

--- project-weightwatcher-app/src/components/gyprockButtons/PBoard.js
+++ project-weightwatcher-app/src/components/gyprockButtons/PBoard.js
@@ -10,14 +10,11 @@ class PBoard extends React.Component {
         }
     }
 
-    //function to increase state weight
-    /*weightIncrease() {
-        this.setState((state, props) => ({
-            count: state.count + props.increment
-          }));
-    } */
-
-    // function for how many multiplier
+  /* multiply(props) {
+       
+       
+       return loadNumber * weightValue
+   } */
 
     componentDidMount() {}
     // logic to add to count state
@@ -28,9 +25,14 @@ class PBoard extends React.Component {
         return (
             <div>
                 <button onClick={() => {
+                    // look into modals this is just a temporary solution!
+                    let loadNumber = Number(prompt("How many Sheets of this type are being loaded?"))
+                    while (isNaN(loadNumber)) {
+                        loadNumber = Number(prompt("Please enter a numerical value for the number of sheets being loaded!"));
+                      }
                     this.setState((state) => ({
-                        count: state.count + 44,
-                        sheetsLoaded: state.sheetsLoaded + 1
+                        count: state.count + (44 * loadNumber),
+                        sheetsLoaded: state.sheetsLoaded + (1 * loadNumber)
                       }));
                 }}>
                     6000x1350
